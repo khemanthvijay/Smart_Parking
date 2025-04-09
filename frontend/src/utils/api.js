594ef7c329954ada2fa04d6a3a4ebc1d6d58b31a
khemanthvijay <khemanthvijay@gmail.com>
2025-04-09 01:35:08 -0700

    updated base url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "/api";
+const API_BASE_URL = "/";
 export const apiRequest = async (endpoint, method = 'GET', body = null, customHeaders = {}) => {
   const url = `${API_BASE_URL}${endpoint}`;
   const options = {
